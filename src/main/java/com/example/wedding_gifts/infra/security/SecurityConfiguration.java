59db7f970064a8b0b5c471929b60d83430722604
BHM871 <adrianalmeida417@gmail.com>
2023-09-25 23:42:09 -0300

    Adding permissition for other endpoints

--- src/main/java/com/example/wedding_gifts/infra/security/SecurityConfiguration.java
+++ src/main/java/com/example/wedding_gifts/infra/security/SecurityConfiguration.java
@@ -24,6 +24,7 @@ public class SecurityConfiguration {
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(HttpMethod.DELETE, "/account/delete").authenticated()
                 .requestMatchers(HttpMethod.POST, "/account/update").authenticated()
+                .anyRequest().permitAll()
             )
             .build();
     }
