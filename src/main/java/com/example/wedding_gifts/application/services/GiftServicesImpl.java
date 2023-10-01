5fb2a4c37b8c46a44a443ab04593c4718ccd5a38
BHM871 <adrianalmeida417@gmail.com>
2023-10-01 19:11:42 -0300

    Adding checks in GiftServices

--- src/main/java/com/example/wedding_gifts/application/services/GiftServicesImpl.java
+++ src/main/java/com/example/wedding_gifts/application/services/GiftServicesImpl.java
@@ -10,8 +10,11 @@ import org.springframework.stereotype.Service;
 import com.example.wedding_gifts.core.domain.dtos.gift.CreateGiftDTO;
 import com.example.wedding_gifts.core.domain.dtos.gift.UpdateGiftDTO;
 import com.example.wedding_gifts.core.domain.dtos.gift.searchers.SearcherByCategoriesAndPriceDTO;
+import com.example.wedding_gifts.core.domain.dtos.gift.searchers.SearcherByCategoriesDTO;
+import com.example.wedding_gifts.core.domain.dtos.gift.searchers.SearcherByPriceDTO;
 import com.example.wedding_gifts.core.domain.dtos.gift.searchers.SearcherByTitleAndCategoriesDTO;
 import com.example.wedding_gifts.core.domain.dtos.gift.searchers.SearcherByTitleAndPriceDTO;
+import com.example.wedding_gifts.core.domain.dtos.gift.searchers.SearcherByTitleDTO;
 import com.example.wedding_gifts.core.domain.dtos.gift.searchers.SearcherDTO;
 import com.example.wedding_gifts.core.domain.model.Gift;
 import com.example.wedding_gifts.core.usecases.gift.GiftRepository;
@@ -52,18 +55,35 @@ public class GiftServicesImpl implements GiftUseCase {
 
         if(searcher.title() != null){
             if(searcher.categories() != null) {
-                return repository.getByTitleAndCategoriesOrBought(new SearcherByTitleAndCategoriesDTO(searcher.title(), searcher.categories(), searcher.isBought() != null ? searcher.isBought() : null));
+                return repository.getByTitleAndCategoriesOrBought(
+                    new SearcherByTitleAndCategoriesDTO(searcher.title(), searcher.categories(), searcher.isBought() != null ? searcher.isBought() : null)
+                );
             }
 
             if(searcher.startPrice() != null || searcher.endPrice() != null) {
                 SearcherByTitleAndPriceDTO newSearcher = searcherByTitleAndPriceDTO(searcher);
                 return repository.getByTitleAndPriceOrBought(newSearcher);
             }
+
+            return repository.getByTitleOrBoutght(
+                new SearcherByTitleDTO(searcher.title(), searcher.isBought() != null ? searcher.isBought() : null)
+            );
         }
 
-        if(searcher.categories() != null && (searcher.startPrice() != null || searcher.endPrice() != null)) {
-            SearcherByCategoriesAndPriceDTO newSearcher = searcherByCategoriesAndPriceDTO(searcher);
-            return repository.getByCategoriesAndPriceOrBought(newSearcher);
+        if(searcher.categories() != null) {
+            if(searcher.startPrice() != null || searcher.endPrice() != null) {
+                SearcherByCategoriesAndPriceDTO newSearcher = searcherByCategoriesAndPriceDTO(searcher);
+                return repository.getByCategoriesAndPriceOrBought(newSearcher);
+            }
+
+            return repository.getByCategoriesOrBought(
+                new SearcherByCategoriesDTO(searcher.categories(), searcher.isBought() != null ? searcher.isBought() : null)
+            );
+        }
+
+        if(searcher.startPrice() != null || searcher.endPrice() != null) {
+            SearcherByPriceDTO newSearcher = searcherByPriceDTO(searcher);
+            return repository.getByPriceOrBought(newSearcher);
         }
 
         throw new Exception("Filters are null");
@@ -146,6 +166,32 @@ public class GiftServicesImpl implements GiftUseCase {
                 searcher.startPrice(), 
                 searcher.endPrice(), 
                 searcher.isBought() != null ? searcher.isBought() : null);
+        }
+
+        return newSearcher;
+    }
+
+    private SearcherByPriceDTO searcherByPriceDTO(SearcherDTO searcher) {
+        SearcherByPriceDTO newSearcher;
+
+        if(searcher.startPrice() == null) {
+            newSearcher = new SearcherByPriceDTO(
+                BigDecimal.ZERO, 
+                searcher.endPrice(),
+                searcher.isBought() != null ? searcher.isBought() : null
+            );
+        } else if(searcher.endPrice() == null) {
+            newSearcher = new SearcherByPriceDTO(
+                searcher.startPrice(), 
+                BigDecimal.valueOf(Double.MAX_VALUE),
+                searcher.isBought() != null ? searcher.isBought() : null 
+            );
+        } else {
+            newSearcher = new SearcherByPriceDTO(
+                searcher.startPrice(), 
+                searcher.endPrice(),
+                searcher.isBought() != null ? searcher.isBought() : null 
+            );
         }
 
         return newSearcher;
