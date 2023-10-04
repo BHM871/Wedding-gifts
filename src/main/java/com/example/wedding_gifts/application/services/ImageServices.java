54cdd9fefd8773bf9a0d007f424cbcd14304653d
BHM871 <adrianalmeida417@gmail.com>
2023-10-04 20:01:32 -0300

    Implementing IImageUseCase

---
+++ src/main/java/com/example/wedding_gifts/application/services/ImageServices.java
@@ -0,0 +1,64 @@
+package com.example.wedding_gifts.application.services;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.wedding_gifts.core.domain.dtos.image.DeleteImageDTO;
+import com.example.wedding_gifts.core.domain.dtos.image.ImageDTO;
+import com.example.wedding_gifts.core.domain.dtos.image.SaveImageDTO;
+import com.example.wedding_gifts.core.domain.model.Image;
+import com.example.wedding_gifts.core.usecases.image.IImageRepository;
+import com.example.wedding_gifts.core.usecases.image.IImageUseCase;
+
+public class ImageServices implements IImageUseCase {
+
+    String sourceImages = "com/exemple/wedding_gifts/infra/images/";
+    
+    @Autowired
+    IImageRepository repository;
+
+    @Override
+    public String saveImage(ImageDTO image) throws Exception {
+        try {
+            byte[] bytesOfImage = image.image().getBytes();
+            Path path = Paths.get(sourceImages+image.accountId()+"/"+image.giftId()+"/"+LocalDateTime.now().toString());
+            Files.write(path, bytesOfImage);
+
+            return repository.saveImage(new SaveImageDTO(path.toString(), image.giftId())); 
+        } catch (Exception e) {
+            throw new Exception("Error in save image", e);
+        }
+    }
+
+    @Override
+    public void deleteImage(DeleteImageDTO deleteImage) throws Exception {
+        boolean isDeleted = Files.deleteIfExists(Paths.get(deleteImage.pathImage()));
+        
+        if(isDeleted){
+            repository.deleteImage(deleteImage);
+        } else {
+            throw new Exception("Image not exists");
+        }
+    }
+
+    @Override
+    public String getById(UUID imageId) throws Exception {
+        return repository.getById(imageId).getPathImage();
+    }
+
+    @Override
+    public List<String> getAllByGift(UUID giftId) throws Exception {
+        List<Image> images = repository.getAllImagesByGift(giftId);
+
+        List<String> pathImages = images.stream().map(image -> image.getPathImage()).toList();
+
+        return pathImages;
+    }
+    
+}
--- src/main/java/com/example/wedding_gifts/core/domain/dtos/image/DeleteImageDTO.java
+++ src/main/java/com/example/wedding_gifts/core/domain/dtos/image/DeleteImageDTO.java
@@ -3,6 +3,7 @@ package com.example.wedding_gifts.core.domain.dtos.image;
 import java.util.UUID;
 
 public record DeleteImageDTO(
+    String pathImage,
     UUID imageId,
     UUID giftId,
     UUID accountId
