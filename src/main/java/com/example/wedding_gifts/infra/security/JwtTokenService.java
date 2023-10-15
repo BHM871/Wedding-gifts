bbfd935c31c9ff2f9f7a2e2f6f4f02e534396f35
BHM871 <adrianalmeida417@gmail.com>
2023-10-14 23:14:20 -0300

    Implementing token persistence

--- src/main/java/com/example/wedding_gifts/infra/security/JwtTokenService.java
+++ src/main/java/com/example/wedding_gifts/infra/security/JwtTokenService.java
@@ -1,5 +1,9 @@
 package com.example.wedding_gifts.infra.security;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +13,9 @@ import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.example.wedding_gifts.adapters.security.ITokenManager;
 import com.example.wedding_gifts.commun.LimitTimeForToken;
+import com.example.wedding_gifts.core.domain.dtos.token.SaveTokenDTO;
 import com.example.wedding_gifts.core.domain.model.Account;
+import com.example.wedding_gifts.core.usecases.token.ITokenUseCase;
 
 @Service
 public class JwtTokenService implements ITokenManager {
@@ -17,6 +23,9 @@ public class JwtTokenService implements ITokenManager {
     @Value("${api.security.token.secret}")
     private String secret;
     private String issuer = "wedding_gifts_api";
+
+    @Autowired
+    private ITokenUseCase tokenService;
 
     @Override
     public String generatorToken(Account account) throws Exception {
@@ -27,7 +36,9 @@ public class JwtTokenService implements ITokenManager {
                 .withSubject(account.getEmail())
                 .withExpiresAt(LimitTimeForToken.genExpirationInstant())
                 .sign(algorithm);
-                
+
+            tokenService.saveToken(new SaveTokenDTO(token, LocalDateTime.ofInstant(LimitTimeForToken.genExpirationInstant(), ZoneId.of("-03:00")), account.getId()));
+
             return token;
         } catch (JWTCreationException e) {
             throw new RuntimeException("Error while generating token", e);
